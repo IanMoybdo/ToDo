b4c0bd92f9abceda22d6ad7b412264e110f30658
andrealozancic <andrealozancic@Andreas-MBP.homenet.telecomitalia.it>
2024-05-24 16:42:27 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ToDo/',
+  base: './',
   plugins: [react()],
+  build: {
+    outDir: 'dist', // this is the default value
+  },
+
 })
